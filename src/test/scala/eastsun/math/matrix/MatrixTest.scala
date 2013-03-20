240293bf9e3abe3855a8abe28409965bca51cfb8
eastsun <eastsun@ymail.com>
2013-03-21 01:03:54 +0800

    add some test cases

--- src/test/scala/eastsun/math/matrix/MatrixTest.scala
+++ src/test/scala/eastsun/math/matrix/MatrixTest.scala
@@ -21,5 +21,8 @@ class ExampleSuite extends FunSuite {
 
     val allElements = m(all)
     assert(allElements === Matrix(81, 1, 1 to 81))
+
+    val subMatrix = m(0 to end by 3, 0 to end by 3)
+    assert(subMatrix === Matrix(3, 3, 1::4::7::28::31::34::55::58::61::Nil))
   }
 }
