b7a3aa0b779e7c51dc3c9361d10fcfcf49e40a65
Eastsun <eastsun@ymail.com>
2013-03-20 22:02:22 +0800

    write a test case for matrix indexing

--- src/test/scala/eastsun/math/matrix/MatrixTest.scala
+++ src/test/scala/eastsun/math/matrix/MatrixTest.scala
@@ -6,13 +6,20 @@ import Matrix._
 class ExampleSuite extends FunSuite {
 
   test("Matrix indexing") {
-    val mat = rand[Int](3, 4).map(_ % 10)
-    val neg = mat >= 0
-    val pos = mat < 0
-    val nat = Matrix.zero[Int](3, 4)
-    nat(neg) = mat(neg) * -1
-    nat(pos) = mat(pos) * -1
-    val sum = mat + nat
-    assert(sum.iterator.forall(0==))
+    val m = Matrix(9, 9, 1 to 81)
+
+    val endOfFirstRow = m(0, end)
+    val endOfFirstCol = m(end, 0)
+
+    assert(endOfFirstRow === 73)
+    assert(endOfFirstCol === 9)
+
+    val firstRow = m(0, all)
+    val firstCol = m(all, 0)
+    assert(firstRow === Matrix(1, 9, 1 to 73 by 9))
+    assert(firstCol === Matrix(9, 1, 1 to 9))
+
+    val allElements = m(all)
+    assert(allElements === Matrix(81, 1, 1 to 81))
   }
 }
